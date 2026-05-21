b88e80ace716ffc95b448f11f9d91f6abf8d37f5
zoubou <your.jzoub03@gmail.com>
2026-05-21 18:36:51 +0300

    Created the SPFP_adder module. Added inputs and 2 registers at the beginning of the circuit to hold the values of a and b.

--- rtl/SPFP_adder.v
+++ rtl/SPFP_adder.v
@@ -1,4 +1,28 @@
-module SPFP_adder (A, B, clk, sum)
+module SPFP_adder (A, B, clk, reset, sum)
+    input [31:0] A, B;
+    input clk, reset;
+    output [31:0] sum;
+    reg [31:0] reg_A, reg_B;
+    reg [31:0] reg_sum;
     
-    
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            reg_A <= 0;
+            reg_B <= 0;
+        end else begin
+            reg_A <= A;
+            reg_B <= B;
+        end
+    end
+
+    // Extract sign, exponent, and mantissa
+    wire sign_A = reg_A[31];
+    wire [7:0] exponent_A = reg_A[30:23];
+    wire [22:0] mantissa_A = reg_A[22:0];
+    wire sign_B = reg_B[31];
+    wire [7:0] exponent_B = reg_B[30:23];
+    wire [22:0] mantissa_B = reg_B[22:0];
+
+
+
 endmodule
